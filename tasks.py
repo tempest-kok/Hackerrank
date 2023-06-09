326484fdce9ad10d009238ba5a343682b93702be
tempest_kok <d.kanafin04@mail.ru>
2023-06-09 08:08:36 +0600

    third commit

--- tasks.py
+++ tasks.py
@@ -1,5 +1,3 @@
-n = int(input())
-if n % 2 != 0 or (n % 2 == 0 and 6 <= n <= 20):
-    print("Weird")
-else:
-    print("Not Weird")
+a = int(input())
+b = int(input())
+print(a + b, a - b, a * b, sep="\n")
